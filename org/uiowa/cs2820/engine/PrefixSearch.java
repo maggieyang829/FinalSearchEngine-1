754905e474565caa82f49e4bc23fd3769aad722f
austinforlife <austin-washburn@uiowa.edu>
2014-12-06 20:11:54 -0600

    changed log

--- org/uiowa/cs2820/engine/PrefixSearch.java
+++ org/uiowa/cs2820/engine/PrefixSearch.java
@@ -15,5 +15,20 @@ public class PrefixSearch implements Search {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	/*/
+	@Override
+	public String[] doSearch() {
+		Node n = Node.findNode(field);
+		String[] R = Identifier.getAllIds(n.identlist);
+		if (R == null) return new String[0];
+		return R;
+	}
+	
+	 public static Node findNode(Field f) {
+		setup();
+		SearchField S = new SearchField(f);  // make custom traverser 
+		D.traverse(S);  // go through the linked list
+		return S.h;     // either null or the found Node
+	}
+/*/
 }
