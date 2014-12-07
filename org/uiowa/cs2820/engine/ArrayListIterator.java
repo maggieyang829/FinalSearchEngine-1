60a6956e17fba4665eb47120bc3d58153c5632a6
kreyes29 <khashi-reyes@uiowa.edu>
2014-12-06 20:08:31 -0600

    Started an iterator class, still more research

---
+++ org/uiowa/cs2820/engine/ArrayListIterator.java
@@ -0,0 +1,23 @@
+package org.uiowa.cs2820.engine;
+import java.util.Iterator;
+
+@SuppressWarnings("hiding")
+public class ArrayListIterator<ArrayList> implements Iterator<String>{
+
+	public Iterator<String> iterator() {
+		return this.iterator();
+	}
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
