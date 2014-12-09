1facaba9808cda2d9be18dc3dc43e48e961749d4
austinforlife <austin-washburn@uiowa.edu>
2014-12-09 17:41:16 -0600

    Added Suffix Search

--- org/uiowa/cs2820/engine/Node.java
+++ org/uiowa/cs2820/engine/Node.java
@@ -72,6 +72,29 @@ class SearchPrefix extends Traverser {
 		  return hitList;
 	  }
 }
+class SearchSuffix extends Traverser {
+	Field field = null;
+	ArrayList<Node> hitList;
+	
+	public SearchSuffix( Field x ) {
+		field = x;
+		hitList = new ArrayList<Node>();
+	}
+	@Override
+	public boolean process(Item I) {
+		Node F = (Node) I;
+		String itemValue = (String) F.Key.getFieldValue();
+		String fieldValue = (String) field.getFieldValue();
+			if (F.Key.getFieldName().equals(field.getFieldName())
+				&& fieldValue.endsWith(itemValue)){
+		    hitList.add(F);
+		    }
+			return true;
+		}
+	  public ArrayList<Node> getLst(){
+		  return hitList;
+	  }
+}
 
 class SearchContains extends Traverser {
 	Field field = null;
---
+++ org/uiowa/cs2820/engine/SuffixSearch.java
@@ -0,0 +1,9 @@
+package org.uiowa.cs2820.engine;
+
+public class SuffixSearch {
+
+	public SuffixSearch() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
