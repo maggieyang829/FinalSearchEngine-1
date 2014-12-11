ca3ea9976878abfe39d77d0115144c823573a5dc
kreyes29 <khashi-reyes@uiowa.edu>
2014-12-11 14:10:10 -0600

    Added another test for ArrayListIterator
    Added a test for ArrayListIterator creating an array list from the
    iterator and checking if the elements of the array list and string array
    match.

--- org/uiowa/cs2820/engine/ArrayListIteratorTest.java
+++ org/uiowa/cs2820/engine/ArrayListIteratorTest.java
@@ -1,6 +1,9 @@
 package org.uiowa.cs2820.engine;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class ArrayListIteratorTest {
@@ -19,4 +22,14 @@ public class ArrayListIteratorTest {
 		assertEquals(I.next(), "1");
 	}
 	
+	@Test
+	public void listTest() {
+		String[] S = "01234567".split("");
+		ArrayListIterator<String> I = new ArrayListIterator<String>(S);
+		ArrayList<String> L = new ArrayList<String>();
+		for(int i=0; i < S.length; i++) {
+			L.add(I.next());
+		}
+		assertEquals(L.get(7), "7");
+	}
 }
