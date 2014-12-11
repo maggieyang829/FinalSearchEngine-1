90e231aea01228756ff895ffc9b2fadcde02d939
kreyes29 <khashi-reyes@uiowa.edu>
2014-12-11 17:27:24 -0600

    Changed toStringArray method in ListIterator

--- org/uiowa/cs2820/engine/ListIterator.java
+++ org/uiowa/cs2820/engine/ListIterator.java
@@ -4,10 +4,12 @@ import java.util.*;
 @SuppressWarnings("hiding")
 public class ListIterator<String> implements Iterator<String>{
 	Iterator<String> I;
+	String[] S;
 	
 	public ListIterator(String[] s) {
 		/*I first converted s to an ArrayList but it didn't support remove()
 		  so I convert s to a Linkedlist instead*/
+		S = s;
 		LinkedList<String> L = new LinkedList<String>(Arrays.asList(s));
 		I = L.iterator();
 	}
@@ -35,12 +37,13 @@ public class ListIterator<String> implements Iterator<String>{
 		return count;
 	}
 	
-	@SuppressWarnings("unchecked")
+	public ArrayList<String> toArrayList() {
+		ArrayList<String> L = new ArrayList<String>();
+		while(hasNext()) L.add(next());
+		return L;
+	}
+	
 	public String[] toStringArray() {
-		String[] S = (String[]) new java.lang.String[length()];
-		for(int i=0; i < length(); i++) {
-			S[i] = next();
-		}
 		return S;
 	}
 }
